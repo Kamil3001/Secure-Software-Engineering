eefa4954714cb36e6f7a5f4fd438a77b8ad94052
Kamil3001 <kamil.cierpisz@ucdconnect.ie>
2020-04-27 17:00:21 +0100

    Some additional session management configs

--- Assignment1/src/main/java/com/mcino/assignment1/AppSecurityConfig.java
+++ Assignment1/src/main/java/com/mcino/assignment1/AppSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -20,6 +21,9 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                .and()
                 .authorizeRequests().antMatchers("/", "/login").permitAll()
                 .and()
                 .formLogin().loginPage("/login").permitAll();
